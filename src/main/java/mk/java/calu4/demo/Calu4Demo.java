12baae8a9821cb5bbbb4651d40bdca0fd2cf1650
Dejan Mladenovski <Dejan.Mladenovski@netcetera.com>
2018-02-25 21:31:12 +0100

    First commit

--- src/main/java/mk/java/calu4/demo/Calu4Demo.java
+++ src/main/java/mk/java/calu4/demo/Calu4Demo.java
@@ -1,4 +1,4 @@
-package com.netcetera.calu4.demo;
+package mk.java.calu4.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
